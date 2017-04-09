97529edc5428c76515fab682f32f0d36e13316fb
semencher <semen.cher@mail.ru>
2017-04-09 16:29:02 +0700

    Добавил геттеры и сеттеры

--- syntacticalanalysis.cpp
+++ syntacticalanalysis.cpp
@@ -2,5 +2,102 @@
 
 SyntacticalAnalysis::SyntacticalAnalysis()
 {
+    current_ =  0;
+    state_ =    NORMAL_WORK;
 
+    ElementL2 elementL2;
+    elementL2.isTerminal =  false;
+    elementL2.sym =         "S";
+    L2_.push(elementL2);
+}
+
+void SyntacticalAnalysis::clear()
+{
+    L1_ = std::stack<ElementL1>();
+    L2_ = std::stack<ElementL2>();
+
+    ElementL2 elementL2;
+    elementL2.isTerminal =  false;
+    elementL2.sym =         "S";
+    L2_.push(elementL2);
+
+    current_ =  0;
+    state_ =    NORMAL_WORK;
+
+    conclusion_ = std::stack<int>();
+}
+
+std::stack<ElementL1> SyntacticalAnalysis::stackL1() const
+{
+    return L1_;
+}
+
+std::stack<ElementL2> SyntacticalAnalysis::stackL2() const
+{
+    return L2_;
+}
+
+QVector<Descriptor> SyntacticalAnalysis::convolution() const
+{
+    return convolution_;
+}
+
+QVector<QString> SyntacticalAnalysis::serviceWords() const
+{
+    return serviceWords_;
+}
+
+QVector<QString> SyntacticalAnalysis::rightSigns() const
+{
+    return rightSigns_;
+}
+
+QVector<QString> SyntacticalAnalysis::identifiers() const
+{
+    return identifiers_;
+}
+
+QVector<QString> SyntacticalAnalysis::constsTable() const
+{
+    return constsTable_;
+}
+
+Grammar SyntacticalAnalysis::grammar() const
+{
+    return grammar_;
+}
+
+std::stack<int> SyntacticalAnalysis::conclusion() const
+{
+    return conclusion_;
+}
+
+void SyntacticalAnalysis::setConvolution(const QVector<Descriptor> & convolution)
+{
+    convolution_ = convolution;
+}
+
+void SyntacticalAnalysis::setServiceWords(const QVector<QString> & serviceWords)
+{
+    serviceWords_ = serviceWords;
+}
+
+void SyntacticalAnalysis::setRightSigns(const QVector<QString> & rightSigns)
+{
+    rightSigns_ = rightSigns;
+}
+
+void SyntacticalAnalysis::setIdentifiers(const QVector<QString> & identifiers)
+{
+    identifiers_ = identifiers;
+}
+
+void SyntacticalAnalysis::setConstsTable(const QVector<QString> & constsTable)
+{
+    constsTable_ = constsTable;
+}
+
+void SyntacticalAnalysis::setGrammar(const Grammar & grammar)
+{
+    grammar_ = grammar;
 }
--- syntacticalanalysis.h
+++ syntacticalanalysis.h
@@ -9,6 +9,31 @@ class SyntacticalAnalysis
 public:
     SyntacticalAnalysis();
 
+    /**
+     * @brief clear очищает полученные данные для дальнейшего использования
+     * объекта.
+     */
+    void clear();
+
+    // Геттеры.
+    std::stack<ElementL1> stackL1() const;
+    std::stack<ElementL2> stackL2() const;
+    QVector<Descriptor> convolution() const;
+    QVector<QString> serviceWords() const;
+    QVector<QString> rightSigns() const;
+    QVector<QString> identifiers() const;
+    QVector<QString> constsTable() const;
+    Grammar grammar() const;
+    std::stack<int> conclusion() const;
+
+    // Сеттеры.
+    void setConvolution(const QVector<Descriptor> & convolution);
+    void setServiceWords(const QVector<QString> & serviceWords);
+    void setRightSigns(const QVector<QString> & rightSigns);
+    void setIdentifiers(const QVector<QString> & identifiers);
+    void setConstsTable(const QVector<QString> & constsTable);
+    void setGrammar(const Grammar & grammar);
+
 private:
     std::stack<ElementL1> L1_;
     std::stack<ElementL2> L2_;
